7d6a2ebbc95f60fb2019d27622d4f0f148ae9f22
cesar <cesar@c>
2023-12-20 00:11:09 +0800

    m372 resolved

--- m372/c/a.c
+++ m372/c/a.c
@@ -2,12 +2,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int group_id = 0;
 int group_cnt = 0;
 
+int ROWS;
+int COLS;
+int* CELLS; // col*COLS+row
+int* MMAP;  // col*COLS+row, to mark path
+#define CELL(c, r) (*(CELLS + ((r) * COLS + (c))))
+#define MCELL(c, r) (*(MMAP + ((r) * COLS + (c))))
 // 如果能連到右邊，回傳1，否則回傳0
-int connect_right(char left, char right) {
+int do_right(char left, char right) {
     if (strchr("XHFL", left) != NULL) {
         if (strchr("XH7J", right)) {
             return 1;
@@ -31,7 +38,7 @@ int connect_right(char left, char right) {
 // }
 
 // 如果能連到下面，回傳1，否則回傳0
-int connected_bottom(char top, char bottom) {
+int do_bottom(char top, char bottom) {
     if (strchr("XI7F", top) != NULL) {
         if (strchr("XILJ", bottom) != NULL) {
             return 1;
@@ -41,7 +48,7 @@ int connected_bottom(char top, char bottom) {
 }
 
 // 給定一個起始點，找出鄰居是否可通
-// int connect_neighbor(int r, int c, int rows, int cols, int cells[rows][cols]) {
+// int do_neighbor(int r, int c, int rows, int cols, int cells[rows][cols]) {
 //     // 上下左右
 //     int neighbors[4][2] = { { r - 1, c }, { r + 1, c }, { r, c - 1 }, { r, c + 1 } };
 //     for (int i = 0; i < 4; i++) {
@@ -57,70 +64,84 @@ int connected_bottom(char top, char bottom) {
 // }
 
 // 給定一個起始點，找出鄰居是否可通
-int connect_neighbor(int r, int c, int rows, int cols, int cells[rows][cols], int sum) {
+int do_neighbor(int c, int r) {
+    MCELL(c, r) = 1;
     int neighbor_cnt = 0;
     int connect_cnt = 0;
-    cells[r][c] = 0;
+    int sum = 1;
     // 上
-    if (r - 1 >= 0 && cells[r - 1][c] != 0) {
-        if (connected_bottom(cells[r - 1][c], cells[r][c])) {
-            cells[r - 1][c] = '0';
-            sum++;
-            sum = connect_neighbor(r - 1, c, rows, cols, cells, sum);
+    if ((r - 1) >= 0 && MCELL(c, r - 1) != 1) {
+        if (do_bottom(CELL(c, r - 1), CELL(c, r))) {
+            // CELL(c, r - 1) = '0';
+            sum += do_neighbor(c, r - 1);
         }
     }
     // 下
-    if (r + 1 < rows && cells[r + 1][c] != 0) {
-        if (connected_bottom(cells[r][c], cells[r + 1][c])) {
-            cells[r + 1][c] = '0';
-            sum++;
-            sum = connect_neighbor(r + 1, c, rows, cols, cells, sum);
+    if ((r + 1) < ROWS && MCELL(c, r + 1) != 1) {
+        if (do_bottom(CELL(c, r), CELL(c, r + 1))) {
+            // CELL(c, r + 1) = '0';
+            sum += do_neighbor(c, r + 1);
         }
     }
-
     // 左
-    if (c - 1 >= 0 && cells[r][c - 1] != 0) {
-        if (connect_right(cells[r][c - 1], cells[r][c])) {
-            cells[r][c - 1] = '0';
-            sum++;
-            sum = connect_neighbor(r, c - 1, rows, cols, cells, sum);
+    if ((c - 1) >= 0 && MCELL(c - 1, r) != 1) {
+        if (do_right(CELL(c - 1, r), CELL(c, r))) {
+            // CELL(c - 1, r) = '0';
+            sum += do_neighbor(c - 1, r);
         }
     }
     // 右
-    if (c + 1 < cols && cells[r][c + 1] != 0) {
-        if (connect_right(cells[r][c], cells[r][c + 1])) {
-            cells[r][c + 1] = '0';
-            sum++;
-            sum = connect_neighbor(r, c + 1, rows, cols, cells, sum);
+    if ((c + 1) < COLS && MCELL(c + 1, r) != 1) {
+        if (do_right(CELL(c, r), CELL(c + 1, r))) {
+            // CELL(c + 1, r) = '0';
+            sum += do_neighbor(c + 1, r);
         }
     }
+    // CELL(c, r) = '0';
 
-    return 0;
+    // printf("cn %d, %d, sum=%d\n", c, r, sum);
+    return sum;
 }
 
-void print_layout(int rows, int cols, int cells[rows][cols]) {
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            printf("%c", cells[r][c]);
+void print_layout() {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            printf("%c", CELL(c, r));
         }
         printf("\n");
     }
 }
 
 int main(int argc, char** argv) {
-    int rows, cols;
-    scanf("%d %d", &rows, &cols);
-    int cells[rows][cols];
-    char str[cols];
-    for (int r = 0; r < rows; r++) {
+    scanf("%d %d", &ROWS, &COLS);
+    CELLS = (int*)malloc(sizeof(int) * COLS * ROWS);
+    MMAP = (int*)malloc(sizeof(int) * COLS * ROWS);
+    memset(MMAP, 0, sizeof(int) * COLS * ROWS);
+    char str[500 + 1];
+    for (int r = 0; r < ROWS; r++) {
         scanf("%s", str);
-        for (int c = 0; c < cols; c++) {
-            cells[r][c] = str[c];
+        for (int c = 0; c < COLS; c++) {
+            CELL(c, r) = str[c];
         }
     }
 
-    connect_neighbor(0, 0, rows, cols, cells, 0);
-    print_layout(rows, cols, cells);
+    int max = 0;
+    for (int c = 0; c < COLS; c++) {
+        for (int r = 0; r < ROWS; r++) {
+            if (MCELL(c, r) == 0 && CELL(c, r) != '0') {
+                // printf("-----------(%d,%d) %d\n", c, r, do_neighbor(c, r));
+                int sum = do_neighbor(c, r);
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+    }
+    printf("%d\n", max);
 
+    // print_layout();
+
+    free(CELLS);
+    free(MMAP);
     return 0;
 }
